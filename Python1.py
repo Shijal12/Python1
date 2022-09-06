d111984e80bd8de9c39cb2a1fec8ec4919073de6
Shijal12 <shijal@fusemachines>
2022-09-06 15:42:46 +0545

    Hello Py

--- Python1.py
+++ Python1.py
@@ -15,3 +15,34 @@ c = float(3)  # z will be 3.0
 print(a)
 print(b)
 print(c)
+
+#many values to multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#one value to multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Global Variables
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#Global
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+print("Python is " + x)
---
+++ Python2.py
@@ -0,0 +1,20 @@
+#Data Types
+# defining strings in Python
+string_1 = 'Hello This is a string'
+print(string_1)
+
+string_2 = "This is also a valid string"
+print(string_2)
+
+# triple quotes string for using multiple lines
+string_3 = '''Hello World,
+This is a Python Code.'''
+print(string_3)
+
+# defining list in Python
+list_1 = [2,4,6,8.0, "even", True] # list of even numbers in Python
+print(list_1)
+
+# defining tuple in Python
+tupl_1 = (1,3,5,7.0, "odd", False)
+print(tupl_1)
