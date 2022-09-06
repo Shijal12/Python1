2c024d7f6230a8664d5c90d4948770db3cfc8556
Shijal12 <shijal@fusemachines>
2022-09-06 15:22:03 +0545

    Hello Py

--- Python1.py
+++ Python1.py
@@ -1,1 +1,17 @@
+#this is a comment
 print("Hello, World!")
+
+#variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Casting
+a = str(3)    # x will be '3'
+b = int(3)    # y will be 3
+c = float(3)  # z will be 3.0
+
+print(a)
+print(b)
+print(c)
