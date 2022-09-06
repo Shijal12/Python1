cc45b676935ed26083ba6bd899218b95c836f99b
Shijal12 <shijal@fusemachines>
2022-09-06 18:51:36 +0545

    Task

---
+++ Operators.py
@@ -0,0 +1,32 @@
+x = 15
+y = 4
+#Arithmetic
+
+print('x + y =',x+y)
+
+#Comparison
+
+print('x > y is',x>y)
+
+#Logical Operators
+x = True
+y = False
+
+print('x and y is',x and y)
+
+#Identity operator
+
+x = 5
+y = 5
+
+print(x is not y)
+
+print(x is y)
+
+# Membership operators
+
+x = 'Hello world!'
+
+print('H' in x)
+
+print('hello' not in x)
