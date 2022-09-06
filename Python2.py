da731653628893cec47aa8fd260a6e3d5c2be860
Shijal12 <shijal@fusemachines>
2022-09-06 15:55:03 +0545

    Data Types

--- Python2.py
+++ Python2.py
@@ -18,3 +18,20 @@ print(list_1)
 # defining tuple in Python
 tupl_1 = (1,3,5,7.0, "odd", False)
 print(tupl_1)
+
+#Dictionaries
+
+# empty dictionary
+dict_1 = {}
+
+# dictionary with integer keys
+dict_type1 = {1: 'Shijal', 2: 'Poudel'}
+
+# dictionary with mixed keys
+dict_type2 = {'name': 'Poudel', 1: [6, 9, 3]}
+
+# using the keyword dict()
+dict_type3 = dict({1:'Shijal', 2:'Poudel'})
+
+# from sequence having each item as a pair
+dict_type4 = dict([(1,'Shijal'), (2,'Poudel')])
